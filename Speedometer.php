ff1ee0850f313d12b61d8dfeef560f2d891dfc84
moreau vianney <95301730+baloolc@users.noreply.github.com>
2022-04-26 09:21:12 +0200

    Add files via upload

--- Speedometer.php
+++ Speedometer.php
@@ -2,13 +2,24 @@
 
 class Speedometer
 {
-    public const KM = 22;
+
     public const MILES = 0.621371;
+    private int $kilometer;
 
 
-    public static function converter(): int
+    public static function converter($kilometer): int
     {
-        return self::KM * self::MILES;
+        return $kilometer * self::MILES;
+    }
+
+    public function getKilometer()
+    {
+        return $this->kilometer;
+    }
+
+    public function setkilometer(int $kilometer): void
+    {
+        return $this->kilometer = $kilometer;
     }
 
 }
